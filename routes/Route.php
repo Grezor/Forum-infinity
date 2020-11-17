e6acd0c30f2d6971dc4a71727d596845437af8e4
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-11-17 16:31:13 +0100

    start routing htaccess

---
+++ routes/Route.php
@@ -0,0 +1,24 @@
+<?php
+namespace Router;
+
+class Route {
+
+    public $path;
+    public $action;
+    public $match;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function matches()
+    {
+
+    }
+
+    public function execute()
+    {
+
+    }
+}
---
+++ routes/Router.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Router;
+
+class Router {
+
+    public $url;
+    // private $routes[];
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function run()
+    {
+        
+    }
+}
